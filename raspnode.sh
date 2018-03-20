e5a61b5d534ab0a75872cb39f8355cad1306ed60
jochemin <jochemin@gmail.com>
2018-03-20 14:07:05 +0100

    Update raspnode.sh

--- raspnode.sh
+++ raspnode.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 ##############################################################################################
-#                                                                                            #
-# jochemin                                                                                   #
-#                                                                                            #
-#Bitcoin + Lightning node installation bash script                                           #
-#                                                                                            #
-#Raspberry + external USB drive required, pendrive recommended                               #
-#                                                                                            #
-#BTC Donations. Thank you!! --> 3FM6FypcrSVhdHh7cpVQMrhPXPZ6zcXeYU                           #
+#                                                                                            
+# jochemin                                                                                   
+#                                                                                            
+#Bitcoin + Lightning node installation bash script                                           
+#                                                                                            
+#Raspberry + external USB drive required, pendrive recommended                               
+#                                                                                            
+#BTC Donations. Thank you!! --> 3FM6FypcrSVhdHh7cpVQMrhPXPZ6zcXeYU                           
+#LN 1000 satoshi --> lnbc10u1pdtzpzjpp5x700xrqxc38qke09e03hu2t8qs7w34rpg5ha2s2yhezq7d8aunwqdqudfhkx6r9d45kugrjv9ehqmn0v3jscqzysxqyspfqt7pe468zm4k8cvpnxlrxhxkgm06yuqd823tkszvh04n065u0742ywvvkywfpetdccwrek9pl689vf02wtcxkfccrlvqqta90ra5kklsq7wv4xf
+#Connect to my LN node --> 02d249db09237f974f1c67775accee37a9d1eb3f04f236dda177f5a5c083094f15@jocheminlnd1.ddns.net:9735
 ##############################################################################################
 
 # Fail on error , debug all lines#############################################################
