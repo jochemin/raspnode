ca4407dab09f9a3baba92b04d4fca2bec16e40d4
jochemin <jochemin@gmail.com>
2018-03-20 13:14:56 +0100

    Add files via upload

--- raspnode.sh
+++ raspnode.sh
@@ -25,6 +25,7 @@ TEXT_GREEN='\e[0;32m'
 user=$(logname)
 userhome='/home/'$user
 FOLD1='/dev/'
+PUBLICIP="$(curl ipinfo.io/ip)"
 ##############################################################################################
 
 # Color functions#############################################################################
@@ -65,8 +66,7 @@ function hd_detect {
 function hd_conf {
     drive=$drive"1"
     if mount | grep $drive > /dev/null;then
-        echo 'lo detecto montado'
-        umount $drive > /dev/null
+        umount -l $drive > /dev/null
     fi
     writeyellow 'Formatting hard disk'
     sudo mkfs.ext4 -F $drive -L BITCOIN
@@ -83,9 +83,156 @@ function hd_conf {
     else
         sudo mount -a
     fi
+    sudo chmod 777 $BTCDIR
     writegreen 'Hard disk configured'
 }
 ##############################################################################################
+
+# CONFIGURE SWAP IN THE HARD DRIVE############################################################
+function swap_conf {
+    writeyellow 'Configuring swap in the hard drive'
+    sudo -u $user mkdir -p /home/$user/.bitcoin/swap
+    dd if=/dev/zero of=/home/$user/.bitcoin/swap/swap.file bs=1M count=2148
+    chmod 600 /home/$user/.bitcoin/swap/swap.file
+    sudo sed -i".bak" "/CONF_SWAPFILE/d" /etc/dphys-swapfile
+    sudo sed -i".bak" "/CONF_SWAPSIZE/d" /etc/dphys-swapfile
+    echo "CONF_SWAPFILE=/home/$user/.bitcoin/swap/swap.file" >> /etc/dphys-swapfile
+    echo "CONF_SWAPSIZE=2048" >> /etc/dphys-swapfile
+    mkswap /home/$user/.bitcoin/swap/swap.file
+    swapon /home/$user/.bitcoin/swap/swap.file
+    echo "/home/$user/.bitcoin/swap/swap.file  none  swap  defaults  0    0" >> /etc/fstab
+    writegreen 'swap configured'
+}
+# USER INPUT##################################################################################
+function user_input {
+    # Do we have an external hard drive?
+    while true; do
+        echo -e $TEXT_YELLOW
+        read -p "Is the hard drive connected? It will be formated. (y/n)" yn
+        case $yn in
+            [Yy]* ) hd_detect;break;; #If we have HD_CONFIG value says to configure it.
+            [Nn]* ) echo "Please connect an USB hard drive an retry";exit;;
+            * ) echo "Please answer yes or no. (y/n)";;
+        esac
+    done
+    echo 'Bitcoin and LND need a RPC user and password, please insert data.'
+    read -p 'Insert username: ' rpcuser
+    read -s -p 'Insert password: (will not be shown) ' rpcpass
+    echo 
+    read -p 'Insert your LND node alias: ' LNALIAS
+    echo -e $TEXT_RESET
+    # Do we have a pendrive for SWAP? IMPROVEMENT?
+    #while true; do
+    #    echo -e $TEXT_YELLOW
+    #    read -p "Will you use a pendrive for SWAP?" yn
+    #    case $yn in
+    #        [Yy]* ) SWAP_CONFIG=pen;break;; #If we have SWAP_CONFIG value says to configure it.
+    #        [Nn]* ) echo "SWAP will be set on external Hard Disk";SWAP_CONFIG=hd;break;; #else we configure swap in the external hard drive.
+    #        * ) echo "Please answer yes or no.";;
+    #    esac
+    #    echo -e $TEXT_RESET
+    #done
+}
+##############################################################################################
+
+# UPDATE RASPBERRY ###########################################################################
+function update_rasp {
+    writeyellow 'Updating Raspberry'
+    sudo apt-get update
+    writegreen 'APT update finished...'
+    sudo apt-get -y dist-upgrade
+    writegreen 'APT distributive upgrade finished...'
+    sudo apt-get -y upgrade
+    writegreen 'APT upgrade finished...'
+    sudo apt-get -y autoremove
+    writegreen 'APT auto remove finished...'
+
+    if [ -f /var/run/reboot-required ]; then
+        writered 'Reboot required!'
+    fi
+}
+##############################################################################################
+
+# INSTALL BERKELEY (4.8.30)###################################################################
+function install_berkeley {
+    writeyellow 'Installing database...'
+    sudo -u $user mkdir -p $userhome/bin
+    cd $userhome/bin
+    sudo -u $user wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+    sudo -u $user tar -xzvf db-4.8.30.NC.tar.gz
+    cd db-4.8.30.NC/build_unix/
+    ../dist/configure --enable-cxx
+    make
+    sudo make install
+    writegreen 'Database installed'
+}
+##############################################################################################
+
+# INSTALL BITCOIN CORE #######################################################################
+function install_bitcoin_core {
+    writeyellow 'Installing Bitcoin Core'
+    rm -fR $userhome/bin/bitcoin
+    cd $userhome/bin
+    git clone https://github.com/bitcoin/bitcoin.git
+    cd bitcoin/
+    ./autogen.sh
+    ./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default
+    make
+    sudo make install
+    writegreen 'Bitcoin Core Installed'
+}
+##############################################################################################
+
+# BUILD BITCOIN.CONF##########################################################################
+function build_bitcoinconf {
+    echo 'zmqpubrawblock=tcp://127.0.0.1:18501' > /home/$user/.bitcoin/bitcoin.conf
+    echo 'zmqpubrawtx=tcp://127.0.0.1:18501' >> /home/$user/.bitcoin/bitcoin.conf
+    echo "rpcuser=$rpcuser" >> /home/$user/.bitcoin/bitcoin.conf
+    echo "rpcpassword=$rpcpass" >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'bcache=100' >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'maxmempool=100' >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'userhd=1' >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'txindex=1' >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'daemon=1' >> /home/$user/.bitcoin/bitcoin.conf
+    echo 'server=1' >> /home/$user/.bitcoin/bitcoin.conf
+}
+##############################################################################################
+
+# INSTALL LND#################################################################################
+function install_lnd {
+    writeyellow 'Installing LND 0.4 BETA'
+    cd $userhome
+    sudo -u $user mkdir -p download
+    cd download
+    wget https://github.com/lightningnetwork/lnd/releases/download/v0.4-beta/lnd-linux-arm-v0.4-beta.tar.gz
+    tar -xzf lnd-linux-arm-v0.4-beta.tar.gz
+    sudo install -m 0755 -o root -g root -t /usr/local/bin lnd-linux-arm-v0.4-beta/*
+}
+##############################################################################################
+
+# BUILD LND CONFIG FILE#######################################################################
+function build_lndconf {
+    sudo -u $user mkdir -p $userhome/.lnd
+    echo 'bitcoin.active=1' > $userhome/.lnd/lnd.conf
+    echo "externalip=$PUBLICIP" >> $userhome/.lnd/lnd.conf
+    echo "alias=$LNALIAS" >> $userhome/.lnd/lnd.conf
+    echo 'color=#1d8c09' >> $userhome/.lnd/lnd.conf
+    echo 'bitcoin.node=bitcoind' >> $userhome/.lnd/lnd.conf
+    echo "bitcoind.rpcuser=$rpcuser" >> $userhome/.lnd/lnd.conf
+    echo "bitcoind.rpcpass=$rpcpass" >> $userhome/.lnd/lnd.conf
+    echo 'bitcoind.zmqpath=tcp://127.0.0.1:18501' >> $userhome/.lnd/lnd.conf
+    #autopilot.active=1 >> $userhome/.lnd/lnd.conf
+    #autopilot.maxchannels=5 >> $userhome/.lnd/lnd.conf
+    #autopilot.allocation=0.6 >> $userhome/.lnd/lnd.conf
+}
+##############################################################################################
+
+# INSTALL PREREQUISITES#######################################################################
+function install_prerequisites {
+    writeyellow 'Installing prerequisites'
+    sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools tmux
+    writegreen 'Prerequisites installed'
+}
 # MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN ###
 # Check if script is launched with sudo#######################################################
 if [ "$(id -u)" -ne 0 ]; then
@@ -99,82 +246,54 @@ clear
 ##############################################################################################
 
 # User input##################################################################################
-# Do we have an external hard drive?
-while true; do
-    echo -e $TEXT_YELLOW
-    read -p "Is the hard drive connected? It will be formated. (y/n)" yn
-    case $yn in
-        [Yy]* ) hd_detect;break;; #If we have HD_CONFIG value says to configure it.
-        [Nn]* ) echo "Please connect an USB hard drive an retry";exit;;
-        * ) echo "Please answer yes or no. (y/n)";;
-    esac
-    echo -e $TEXT_RESET
-done
-
-# Do we have a pendrive for SWAP?
-while true; do
-    echo -e $TEXT_YELLOW
-    read -p "Will you use a pendrive for SWAP?" yn
-    case $yn in
-        [Yy]* ) SWAP_CONFIG=pen;break;; #If we have SWAP_CONFIG value says to configure it.
-        [Nn]* ) echo "SWAP will be set on external Hard Disk";SWAP_CONFIG=hd;break;; #else we configure swap in the external hard drive.
-        * ) echo "Please answer yes or no.";;
-    esac
-    echo -e $TEXT_RESET
-done
+user_input
 ##############################################################################################
 
 # Prerequisites###############################################################################
 writegreen 'Script will begin the installation, take a rest.'
 writegreen '____________________________________________________'
-writeyellow 'Updating Raspberry'
-sudo apt-get update
-writegreen 'APT update finished...'
-sudo apt-get -y dist-upgrade
-writegreen 'APT distributive upgrade finished...'
-sudo apt-get -y upgrade
-writegreen 'APT upgrade finished...'
-sudo apt-get -y autoremove
-writegreen 'APT auto remove finished...'
-
-if [ -f /var/run/reboot-required ]; then
-    writered 'Reboot required!'
-fi
-
-writeyellow 'Installing prerequisites'
-sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools tmux
-writegreen 'Prerequisites installed'
+update_rasp
+install_prerequisites
 ##############################################################################################
 
 # Configure external hard drive###############################################################
 if [ "$DRIVE_CONF" = "true" ]; then
     hd_conf
+    swap_conf
 fi
 ##############################################################################################
 
-# Install Berkeley-db 4.8.30##################################################################
-writeyellow 'Installing database...'
-sudo -u $user mkdir -p $userhome/bin
-cd $userhome/bin
-sudo -u $user wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-sudo -u $user tar -xzvf db-4.8.30.NC.tar.gz
-cd db-4.8.30.NC/build_unix/
-../dist/configure --enable-cxx
-make
-sudo make install
-writegreen 'Database installed'
+# INSTALL DATABASE (BERKELEY 4.8.30)##########################################################
+install_berkeley
 ##############################################################################################
 
-# Install Bitcoin Core########################################################################
-writeyellow 'Installing Bitcoin Core'
-rm -R $userhome/bin/bitcoin
-cd $userhome/bin
-git clone https://github.com/bitcoin/bitcoin.git
-cd bitcoin/
-./autogen.sh
-./configure CPPFLAGS="-I/usr/local/BerkeleyDB.4.8/include -O2" LDFLAGS="-L/usr/local/BerkeleyDB.4.8/lib" --enable-upnp-default
-make
-sudo make install
-writegreen 'Bitcoin Core Installed'
+# INSTALL BITCOIN CORE #######################################################################
+install_bitcoin_core
+build_bitcoinconf
 ##############################################################################################
+
+#INSTALL LND##################################################################################
+install_lnd
+build_lndconf
+##############################################################################################
+
+# START BITCOIND##############################################################################
+writeyellow 'Starting Bitcoin Core'
+sudo -u $user bitcoind &
+writegreen 'Bitcoin Core started'
+##############################################################################################
+
+# START LND ##################################################################################
+# Wait 20 minutes for bitcoind to warm up
+writeyellow 'Waiting 20 minutes to start LND'
+sleep 20m
+sudo -u $user tmux new-session -d -s LND
+sudo -u $user tmux send-keys -t LND "lnd --bitcoin.mainnet" Enter
+writegreen 'LND started in tmux session'
+##############################################################################################
+
+#THE END #####################################################################################
+writegreen 'Please create a new Lightning wallet with lncli create.'
+writegreen 'To enter LND tmux session --> tmux a LND'
+writegreen 'Now you have to wait until syncing. Patience.'
 # MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN - MAIN ###
